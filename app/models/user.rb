1388a1d4333412db0a349b8bdab8584df7ff9d9b
Sankhlavinay <vinaysankhla4233@gmail.com>
2024-10-22 11:30:24 +0530

    Event model generate

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :email, :role, presence: true
+
+
+  def organizer?
+    role == 'organizer'
+  end
+
+  def attendee?
+    role == 'attendee'
+  end
 end
